cd74e8e1891fe38e85f60595cf2f4668422b427e
John Isom <John.Isom2001@gmail.com>
2019-11-18 10:50:24 -0700

    Add more skeleton html/css

--- time_manager.rb
+++ time_manager.rb
@@ -4,6 +4,12 @@ require 'sinatra'
 require 'sinatra/content_for'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
+require 'securerandom'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
 
 get '/' do
   erb :index
@@ -15,4 +21,20 @@ end
 
 get '/signup' do
   erb :signup
+end
+
+get '/signout' do
+  erb :signout
+end
+
+post '/signin' do
+  redirect '/'
+end
+
+post '/signup' do
+  redirect '/'
+end
+
+post '/signout' do
+  redirect '/'
 end
