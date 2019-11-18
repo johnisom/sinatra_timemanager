2847abda93f5a9db9d4d8ab361fa612dd8025269
John Isom <John.Isom2001@gmail.com>
2019-11-18 09:42:57 -0700

    Make basic html/css skeleton

--- time_manager.rb
+++ time_manager.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/content_for'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
 get '/' do
   erb :index
+end
+
+get '/signin' do
+  erb :signin
+end
+
+get '/signup' do
+  erb :signup
 end
