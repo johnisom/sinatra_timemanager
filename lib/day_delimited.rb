858b6fc68f15acdf445c6d802e628b76eeef1cb3
John Isom <John.Isom2001@gmail.com>
2019-11-26 10:08:08 -0700

    Satisfy rubocop

--- lib/day_delimited.rb
+++ lib/day_delimited.rb
@@ -5,21 +5,15 @@ module DayDelimited
   private
 
   def day_delimited(from, to)
-    days = days(from, to).map
+    days = days(from, to)
     day_seconds = days.map { |sessions| sessions.sum(&:seconds) }
 
     tot_sec = day_seconds.sum
     avg_sec = tot_sec / (from - to + 1)
 
-    content = days.zip(day_seconds).map do |sessions, seconds|
-      sessions.map do |session|
-        %(<div class="session">#{session.to_html}</div>)
-      end.join + day_delimiter(seconds)
-    end.join
-
     timeframe_html(from, to) +
       choice_html('DAY DELIMITED') +
-      content +
+      day_delimited_content(days, day_seconds) +
       summaries_html(avg_sec, tot_sec)
   end
 
@@ -34,5 +28,13 @@ module DayDelimited
         <div class="line">#{'=' * 150}</div>
       </div>
     HTML
+  end
+
+  def day_delimited_content(days, day_seconds)
+    days.zip(day_seconds).map do |sessions, seconds|
+      sessions.map do |session|
+        %(<div class="session">#{session.to_html}</div>)
+      end.join + day_delimiter(seconds)
+    end.join
   end
 end
