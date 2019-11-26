50b724570dc57a567decee2a2d9ed8d0584b95ca
John Isom <John.Isom2001@gmail.com>
2019-11-26 12:55:53 -0700

    Add week delimited view option

--- lib/week_delimited.rb
+++ lib/week_delimited.rb
@@ -1,8 +1,56 @@
 # frozen_string_literal: true
 
+require_relative 'timeable'
+
 # Week Delimited viewing module
 module WeekDelimited
   private
 
-  def week_delimited(from, to); end
+  include Timeable
+
+  def week_delimited(from, to)
+    weeks_days = weeks(from, to).map do |sessions|
+      sessions.chunk { |session| session.start.time.day }.map(&:last)
+    end
+    weeks_days_seconds = weeks_days.map do |days|
+      days.map { |sessions| sessions.sum(&:seconds) }
+    end
+    weeks_seconds = weeks_days_seconds.map { |days| days.sum }
+
+    tot_sec = weeks_days_seconds.sum(&:sum)
+    avg_sec = begin tot_sec / weeks_days.size
+              rescue ZeroDivisionError; 0
+              end
+
+    timeframe_html(from, to) +
+      choice_html('WEEK DELIMITED') +
+      week_delimited_content(weeks_days, weeks_days_seconds, weeks_seconds) +
+      summaries_html(avg_sec, tot_sec, 'per logged week')
+  end
+
+  def week_delimiter(seconds)
+    <<~HTML
+      <div class="delimiter">
+        <div class="line">#{'=' * 150}</div>
+        <div class="summary">
+          <span class="title">Weekly amount: </span>
+          <span class="content">#{time_elapsed(seconds)}</span>
+        </div>
+        <div class="line">#{'=' * 150}</div>
+      </div>
+    HTML
+  end
+
+  def week_delimited_content(weeks_days, weeks_days_seconds, weeks_seconds)
+    weeks_days.zip(weeks_days_seconds, weeks_seconds).map do |days, days_seconds, weekly_seconds|
+      days.zip(days_seconds).map do |sessions, daily_seconds|
+        <<~HTML
+          <div class="summary">
+            <span class="title">#{strfdate(sessions[0].start.time.to_date)}</span>
+            <span class="content">#{time_elapsed(daily_seconds)}</span>
+          </div>
+        HTML
+      end.join + week_delimiter(weekly_seconds)
+    end.join
+  end
 end
